cbc198aaec8ed14333192875388a10aa8cba3ceb
eaglefree007 <75333835+eaglefree007@users.noreply.github.com>
2023-02-04 05:55:39 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,7 @@
 export function About() {
   return <> <h1>H!!' It's my About PagE</h1>
     <br />
+    <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="HORIZONTAL" data-vanity="eaglefree007" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/eaglefree007?trk=profile-badge">Abhey Kumar</a></div>
+              
     <a >eaglefree007</a></> 
 }
